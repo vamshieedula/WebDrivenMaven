e3ebaf9fdc64b2005c32e06125b923e0e164c031
vamshi <vamshi1608@gmail.com>
2021-08-07 12:12:40 +0530

    Initial Commit

---
+++ src/test/java/mavenWebdriver/LoginTest.java
@@ -0,0 +1,40 @@
+package mavenWebdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+		
+		public static WebDriver driver;
+		
+		@BeforeSuite
+		public void setup() {
+			
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			
+		}
+		
+		@Test
+		public void doLogin() {
+			
+			driver.get("http://gmail.com");
+			driver.findElement(By.id("identifierId")).sendKeys("vamshi1608@gmail.com");
+			driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
+			
+		}
+		
+		@AfterSuite
+		public void tearDown() {
+			
+			driver.quit();
+		}
+	
+
+}
