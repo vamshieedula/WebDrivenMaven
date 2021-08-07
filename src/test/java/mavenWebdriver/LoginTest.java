9fc7884acb19402270e0076f93d7cfceedfc23cc
vamshi <vamshi1608@gmail.com>
2021-08-07 12:29:47 +0530

    Committing the changes made to LoginTest

--- src/test/java/mavenWebdriver/LoginTest.java
+++ src/test/java/mavenWebdriver/LoginTest.java
@@ -27,7 +27,7 @@ public class LoginTest {
 			driver.get("http://gmail.com");
 			driver.findElement(By.id("identifierId")).sendKeys("vamshi1608@gmail.com");
 			driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
-			
+			driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("vamshi1608");
 		}
 		
 		@AfterSuite
